4d6a4835082738580bdd732745c8acb60fe3e9b3
TakioN <jungbeen0724@gmail.com>
2025-05-11 03:12:34 +0900

    feat: add Home page

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,21 @@
+import logo from "@/assets/images/logo.png";
+
+const Profile = () => {
+  return (
+    <div className="absolute top-1/2 -translate-y-1/2 right-2 flex gap-2">
+      <p className="flex items-center font-semibold text-lg">UserName</p>
+      <div className="bg-[#a1a1a1] size-10 rounded-[50%]"></div>
+    </div>
+  );
+};
+
+function Header({ className }) {
+  return (
+    <header className={`w-full p-2 relative ${className}`}>
+      <img src={logo} alt="logo" className="h-[3rem]" />
+      <Profile className="bg-[#446133]" />
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,40 @@
+import { useRef } from "react";
+import Header from "../components/common/header";
+
+import pdfImg from "@/assets/images/pdf.svg";
+
 function Home() {
-  return <div>Home</div>;
+  const fileInputRef = useRef();
+
+  const handleFileSelector = () => {
+    fileInputRef.current?.click();
+  };
+
+  return (
+    <div>
+      <Header />
+      <main className="px-3">
+        <p className="text-start font-bold text-xl py-10">
+          이정빈님 안녕하세요
+        </p>
+        <div className="flex flex-col justify-center items-center border border-[#EB757B] bg-[#eeeeee] aspect-square rounded-md mb-20">
+          <img src={pdfImg} alt="pdf" className="size-25 mb-10" />
+          <div>
+            <input type="file" ref={fileInputRef} className="hidden" />
+            <button
+              className="text-2xl border border-black p-2 rounded-md"
+              onClick={handleFileSelector}
+            >
+              파일 찾아보기
+            </button>
+          </div>
+        </div>
+        <button className="w-4/5 rounded-md h-15 text-2xl font-bold text-white bg-[#275D7A] mb-10">
+          마이페이지
+        </button>
+      </main>
+    </div>
+  );
 }
 
 export default Home;
