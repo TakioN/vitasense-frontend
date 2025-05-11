cc24eb30b44b280bf1f11ffd96c3ea4314d72b16
TakioN <jungbeen0724@gmail.com>
2025-05-11 14:16:00 +0900

    feat: add recommend page

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import UserSetting from "./pages/UserSetting";
 import Result from "./pages/Result";
+import Recommend from "./pages/Recommend";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserSetting />} />
+        <Route path="/" element={<Recommend />} />
       </Routes>
     </Router>
   );
